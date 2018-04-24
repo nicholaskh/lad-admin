1706361bd335537acf4c1bdc8e93cd5804a37575
xiangpeng2016 <m18281885253@163.com>
2018-04-24 09:53:04 +0800

    新建实体类添加注释

--- src/main/java/com/lad/admin/infor/model/DailNewsBo.java
+++ src/main/java/com/lad/admin/infor/model/DailNewsBo.java
@@ -2,6 +2,13 @@ package com.lad.admin.infor.model;
 
 import java.io.Serializable;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@Document(collection="dailynews")
 public class DailNewsBo extends ResultBo implements Serializable {
 
 }
--- src/main/java/com/lad/admin/infor/model/YanglaoBo.java
+++ src/main/java/com/lad/admin/infor/model/YanglaoBo.java
@@ -2,6 +2,13 @@ package com.lad.admin.infor.model;
 
 import java.io.Serializable;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@Document(collection="yanglao")
 public class YanglaoBo extends ResultBo implements Serializable {
 
 }
